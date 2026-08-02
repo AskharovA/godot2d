54a2a50a56ed81be75f615ffe0c9f9b3c81b9784
Akylbek Askharov <androidepta@gmail.com>
2026-08-02 19:37:32 +0500

    alien-attack: add player motion script

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -1,1 +1,17 @@
 extends CharacterBody2D
+
+var value = 500
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2(0, 0)
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x = value
+	if Input.is_action_pressed("move_left"):
+		velocity.x = 0-value
+	if Input.is_action_pressed("move_up"):
+		velocity.y = 0-value
+	if Input.is_action_pressed("move_down"):
+		velocity.y = value
+
+	move_and_slide()
