8c2a43844d8205396884e58be47801fe8e9d20b0
Akylbek Askharov <androidepta@gmail.com>
2026-08-03 17:14:53 +0500

    alien-attack: clamping player's position

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var value = 500
+var value = 1000
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(0, 0)
@@ -15,3 +15,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = value
 
 	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+
+	#if global_position.x < 0:
+		#global_position.x = 0
+	#if global_position.x > screen_size.x:
+		#global_position.x = screen_size.x
+	#if global_position.y < 0:
+		#global_position.y = 0
+	#if global_position.y > screen_size.y:
+		#global_position.y = screen_size.y
+	
+	#global_position.x = clampf(global_position.x, 0, screen_size.x)
+	#global_position.y = clampf(global_position.y, 0, screen_size.y)
+	
+	global_position = global_position.clamp(Vector2(0, 0), screen_size)
