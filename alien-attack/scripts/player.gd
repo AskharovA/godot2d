e2e1036af01ec3f11c1117ddc1335134b191134e
Akylbek Askharov <androidepta@gmail.com>
2026-08-04 16:18:03 +0500

    alien-attack: shooting rockets

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -1,33 +1,30 @@
 extends CharacterBody2D
 
-var value = 1000
+var speed = 1000
+const rcene = preload("res://scenes/rocket.tscn")
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(0, 0)
 
 	if Input.is_action_pressed("move_right"):
-		velocity.x = value
+		velocity.x = speed
 	if Input.is_action_pressed("move_left"):
-		velocity.x = 0-value
+		velocity.x = -speed
 	if Input.is_action_pressed("move_up"):
-		velocity.y = 0-value
+		velocity.y = -speed
 	if Input.is_action_pressed("move_down"):
-		velocity.y = value
+		velocity.y = speed
 
 	move_and_slide()
 	
 	var screen_size = get_viewport_rect().size
-
-	#if global_position.x < 0:
-		#global_position.x = 0
-	#if global_position.x > screen_size.x:
-		#global_position.x = screen_size.x
-	#if global_position.y < 0:
-		#global_position.y = 0
-	#if global_position.y > screen_size.y:
-		#global_position.y = screen_size.y
-	
-	#global_position.x = clampf(global_position.x, 0, screen_size.x)
-	#global_position.y = clampf(global_position.y, 0, screen_size.y)
-	
 	global_position = global_position.clamp(Vector2(0, 0), screen_size)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot() -> void:
+	var rinstance := rcene.instantiate() as Area2D
+	rinstance.global_position.x += 60
+	add_child(rinstance)
