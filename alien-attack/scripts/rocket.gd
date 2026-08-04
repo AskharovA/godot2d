96fd1ad818d940f63365c67dea8a65ebbc7fa680
Akylbek Askharov <androidepta@gmail.com>
2026-08-04 15:51:03 +0500

    alien-attack: use delta for rocket speed

--- alien-attack/scripts/rocket.gd
+++ alien-attack/scripts/rocket.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 10
+@export var speed: float = 120.0
 
 func _physics_process(delta: float) -> void:
-	global_position.x += speed
+	global_position.x += speed * delta
