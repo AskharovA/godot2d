871f68a67e0a27c3809e84b75d3dfbad426ea8cb
Akylbek Askharov <androidepta@gmail.com>
2026-08-05 13:48:41 +0500

    alien-attack: spawn enemy function

--- alien-attack/scripts/enemy_spawner.gd
+++ alien-attack/scripts/enemy_spawner.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
 @onready var timer = $Timer
+@onready var spawn_positions = $SpawnPositions
+const enemy_scene = preload("res://scenes/enemy.tscn")
 
 func _ready() -> void:
 	timer.connect("timeout", _on_timeout)
 
 func _on_timeout() -> void:
-	print("Timeout")
+	spawn_enemy()
+
+func spawn_enemy() -> void:
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
+	var enemy_instance := enemy_scene.instantiate() as Area2D
+	enemy_instance.global_position = random_spawn_position.global_position
+	add_child(enemy_instance)
